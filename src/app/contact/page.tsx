9cdb39d0e9c690da846735f0919e6a4d3cf72b61
Jay200123 <kenken.ayonon@gmail.com>
2025-02-12 20:13:08 +0800

    Add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <h3 className="md:text-lg text-sm md:font-bold font-medium">
+        This is Contact Page
+      </h3>
+    </div>
+  );
+}
