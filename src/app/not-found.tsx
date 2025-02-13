2bbd276198b96f380bbfaa0f1bd3c66d5c1a6756
Jay200123 <kenken.ayonon@gmail.com>
2025-02-13 13:44:50 +0800

    Add NotFound component to handle 404 errors

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main>
+      <h1>404</h1>
+      <h3>Page Not Found</h3>
+      <p>The resource requested was not found!</p>
+    </main>
+  );
+}
