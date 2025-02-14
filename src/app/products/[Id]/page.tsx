8fa050fd9a0edd3bc67b5f75d1935613bb4b2a51
Jay200123 <kenken.ayonon@gmail.com>
2025-02-14 20:23:44 +0800

    Add metadata generation for product details page

--- src/app/products/[Id]/page.tsx
+++ src/app/products/[Id]/page.tsx
@@ -1,10 +1,19 @@
-export default function ProductDetails({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: {
     Id: string;
   };
-}) {
+};
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Product ${params?.Id}`,
+    description: `Product Details`,
+  };
+};
+
+export default function ProductDetails({ params }: Props) {
   return (
     <>
       <h3>Product Details with {params?.Id}</h3>
