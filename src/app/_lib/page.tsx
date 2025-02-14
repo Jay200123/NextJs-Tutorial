243d17a2477564e1cfb647f1e0e5c784e099a667
Jay200123 <kenken.ayonon@gmail.com>
2025-02-14 20:23:29 +0800

    Add PrivateRoute component to handle access control for private routes

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,6 @@
+// the underscore in the folder name is a 
+// convention to indicate that this is a private folder
+
+export default function PrivateRoute() {
+  return <>Warning this is a private route</>;
+}
