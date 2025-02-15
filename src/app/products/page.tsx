6693ab08f7f04a27d2011adb71bf97500f9d7d27
Jay200123 <kenken.ayonon@gmail.com>
2025-02-15 14:49:57 +0800

    Add Go Back button to Products page for improved navigation

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 export default function Products() {
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.replace("/");
+  };
   return (
     <>
       <h1>Products Main Page</h1>
+      <button onClick={handleBack}>Go Back</button>
     </>
   );
 }
