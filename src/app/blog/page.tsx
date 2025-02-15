637b0a064b6ae92bd72d926d49a1f11350b796d2
Jay200123 <kenken.ayonon@gmail.com>
2025-02-15 14:49:39 +0800

    Add error handling to BlogPage component

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,14 @@
+import { tr } from "framer-motion/client";
+
 export default function BlogPage() {
+  const boolValue = true;
+
+  const isError = boolValue ? true : false; 
+
+  if(isError) {
+    throw new Error("An error occurred"); 
+  }
+  
   return (
     <>
       <h1>Main Blog Page</h1>
