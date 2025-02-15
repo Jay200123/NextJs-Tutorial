29913f49793b0a8d831804de947f62f6c81af163
Jay200123 <kenken.ayonon@gmail.com>
2025-02-15 14:49:25 +0800

    Add back button to Albums page for navigation

--- src/app/albums/page.tsx
+++ src/app/albums/page.tsx
@@ -1,7 +1,15 @@
-export default function (){
-    return(
-        <>
-        <h3>This is Albums page!</h3>
-        </>
-    )
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function () {
+  const router = useRouter();
+  const handleBack = () => {
+    router.replace("/");
+  };
+  return (
+    <>
+      <h3>This is Albums page!</h3>
+      <button onClick={handleBack}>Go Back</button>
+    </>
+  );
 }
